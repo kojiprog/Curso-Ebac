8a12d3641aecdf4f297b3a5995aba3f3d9d45822
kojiprog <alexandre.k.ueda@gmail.com>
2024-04-29 20:41:41 -0300

    Validação JS OK

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
-let numero_box01 = document.querySelectorAll('#box01');
-let numero_box02 = document.querySelectorAll('#box02');
+function verificador() {
+  const numero_box01 = document.querySelector("#box01");
+  const numero_box02 = document.querySelector("#box02");
+  const resultadoHTML = document.querySelector("#resultado");
+  const box01 = parseInt(numero_box01.value);
+  const box02 = parseInt(numero_box02.value);
 
-function verificador(numero_box01, numero_box02){
-    
+  if (numero_box01.value == "" || numero_box02.value == "") {
+    const resposta = "Por favor preencha os campos corretamente.";
+    return (resultadoHTML.innerHTML = resposta);
+  } else if (box01 > box02) {
+    const resposta = `O número ${box01} é maior que o número ${box02}`;
+    return (resultadoHTML.innerHTML = resposta);
+  } else if (box01 < box02) {
+    const resposta = `O número ${box02} é maior que o número ${box01}`;
+    return (resultadoHTML.innerHTML = resposta);
+  } else {
+    const resposta = `Os números são iguais.`;
+    return (resultadoHTML.innerHTML = resposta);
+  }
 }
